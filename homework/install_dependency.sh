ab8a4fbae419022216e11d481f71fe67784ff27f
陈钰浩 <chenyh4@g-bits.com>
2024-11-28 21:53:49 +0800

    feat: 作业01（未完成仅提交工程）

--- homework/install_dependency.sh
+++ homework/install_dependency.sh
@@ -6,14 +6,17 @@ cd `dirname $0`
 cd ./eigen
 
 echo "Installing dependencies..."
-sudo apt-get install cmake
-sudo apt-get install g++
+sudo apt-get install -y cmake g++ git
+
+echo "Installing OpenCV..."
+sudo apt-get install -y build-essential cmake git libgtk-3-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev openexr libatlas-base-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev python3-dev python3-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev gfortran
+sudo apt-get install -y libopencv-dev
 
 echo "Building..."
 mkdir -p ${FILE_PATH}/
 cd ${FILE_PATH}/
 cmake ..
-make
+make -j8
 sudo make install
 cd ../..
 
